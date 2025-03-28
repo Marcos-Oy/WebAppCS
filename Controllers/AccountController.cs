1e6e6f16aed1b6a834c1f8cbc121119c55f3188a
Marcos Oyarzo <oyarzo.marcos97@outlook.com>
2025-03-27 23:20:31 -0300

    Initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebAppCS.Models;
+
+namespace WebAppCS.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // GET: /Account/Login
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+        
+        // GET: /Account/ForgotPassword
+        public IActionResult ForgotPassword()
+        {
+            return View("ForgotPassword");
+        }
+
+         // GET: /Account/Register
+        public IActionResult Register()
+        {
+                return View("Register");
+        }
+
+        [HttpPost] // Simula el submit del formulario
+        public IActionResult FakeLogin()
+        {
+            // Redirige directamente al Home SIN validar
+            return RedirectToAction("Index", "Home");
+        }
+        
+    }
+}
