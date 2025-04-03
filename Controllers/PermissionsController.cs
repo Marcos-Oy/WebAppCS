c18c1b6f7532cdde242c8797d33845cd3842c65d
Marcos Oyarzo <oyarzo.marcos97@outlook.com>
2025-04-02 23:14:46 -0300

    Control Rol y Permisos
    Cuando se crea un rol se crean los permisos para que se puedan habilitar y cuando se elimina un rol, también se eliminan los permisos

--- Controllers/PermissionsController.cs
+++ Controllers/PermissionsController.cs
@@ -26,10 +26,24 @@ namespace WebAppCS.Controllers
         [HttpPost]
         public IActionResult Insert(Roles model)
         {
-            // Consulta SQL con interpolación
-            string query = $"INSERT INTO Roles (Nombre) VALUES ('{model.Nombre}')";
-            // Ejecutar el comando de inserción
-            _database.EjecutarComando(query);
+            // Consulta SQL para insertar el rol
+            string insertQuery = $"INSERT INTO Roles (Nombre) VALUES ('{model.Nombre}')";
+            _database.EjecutarComando(insertQuery);
+
+            // Obtener el último ID insertado
+            string selectQuery = "SELECT LAST_INSERT_ID();";
+            DataTable resultTable = _database.EjecutarConsulta(selectQuery) as DataTable;
+            
+            if (resultTable != null && resultTable.Rows.Count > 0)
+            {
+                int insertedId = Convert.ToInt32(resultTable.Rows[0][0]);
+
+                string moduloUsuario = $"INSERT INTO Permisos (id_rol, id_modulo) VALUES('{insertedId}', 1)";
+                _database.EjecutarComando(moduloUsuario);
+
+                string moduloProductos = $"INSERT INTO Permisos (id_rol, id_modulo) VALUES('{insertedId}', 1)";
+                _database.EjecutarComando(moduloProductos);
+            }
 
             return RedirectToAction("Index", "Permissions");
         }
@@ -51,8 +65,12 @@ namespace WebAppCS.Controllers
         [HttpPost]
         public IActionResult Delete(int id)
         {
-            string query = $"DELETE FROM Roles WHERE Id = {id}";
-            _database.EjecutarComando(query);
+            string queryRole = $"DELETE FROM Roles WHERE Id = {id}";
+            _database.EjecutarComando(queryRole);
+
+            string queryPermission = $"DELETE FROM Permisos WHERE id_rol = {id}";
+            _database.EjecutarComando(queryPermission);
+
             return RedirectToAction("Index", "Permissions");
         }
 
